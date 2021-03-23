cd9858fc2aa00ab7c45fffdfcb067d223a23c91e
manfelt <35919896+manfelt@users.noreply.github.com>
2021-03-23 22:30:45 +0100

    Legger til modellfunksjon

---
+++ src/AppModell.vue
@@ -0,0 +1,18 @@
+<script>
+import API from './adapter.js'
+
+export default {
+    TildelSted(Sted) {
+		this.Sted = Sted;
+        API.hentGeoKoordinater(Sted);
+        this.refresh();
+    },
+    refresh() {
+        API.hentSamtidsVaerData()
+        .then((result) => {
+            console.log(result);
+        })
+    }
+}
+
+</script>
